e0dcd3bfa6ac39e4d7d9cb46f010a4f1fe5e37ef
petra_strucic <petra.strucic@outlook.com>
2026-09-11 12:58:10 +0200

    init database PoC

---
+++ Cart service/PepperShop.Cart.API/Utilities/HostingExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace PepperShop.Cart.API.Utilities
+{
+    internal static class HostingExtensions
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication()
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "https://localhost:5001";
+                    options.TokenValidationParameters.ValidateAudience = false;
+                });
+
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("CartServiceScope", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "cartService");
+                });
+            });
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            return builder.Build();
+        }
+    }
+}
--- Cart service/PepperShop.Cart.Data/Entities/Cart.cs
+++ Cart service/PepperShop.Cart.Data/Entities/Cart.cs
@@ -1,6 +1,31 @@
 ﻿namespace PepperShop.Cart.Data.Entities
 {
-    internal class Cart
+    public class Cart
     {
+        public string Id { get; set; }
+
+        public List<CartItem>? Items { get; set; }
+
+        public decimal Discount { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public Cart(string id,
+            List<CartItem>? items = null,
+            decimal discount = 0,
+            decimal totalPrice = 0)
+        {
+            Id = id;
+            Items = items;
+            Discount = discount;
+            TotalPrice = totalPrice;
+        }
+    }
+
+    public class CartItem()
+    {
+        public string ProductId { get; set; }
+
+        public int Quantity { get; set; }
     }
 }
